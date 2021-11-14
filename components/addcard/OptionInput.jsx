ecffa47cf034e80dec083ca73f7d59ba63da2bc5
Denis Lindner <lindnerdenis@web.de>
2021-11-14 20:03:52 +0100

    use react-hook-form to creat submit function for options

--- components/addcard/OptionInput.jsx
+++ components/addcard/OptionInput.jsx
@@ -1,19 +1,34 @@
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
-
-export const OptionInput = ({ handleRemoveInput }) => {
+export const OptionInput = ({ handleRemoveInput}) => {
+  const { register, handleSubmit } = useForm();
+  let userOptions = [];
+  const onSubmit = (data) => {
+    const userOption= JSON.stringify(data);
+    userOptions.push(userOption);
+    console.log(userOptions);
+  };
+  
   return (
     <>
       <StyledList>
-        <img
-          src="/Icon/minusCircle.svg"
-          alt="delete option"
-          width="30px"
-          height="30px"
-          onClick={handleRemoveInput}
-        />
-        <StyledForm>
-          <input type="text" name="newQption" placeholder="write option here" />
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            <img
+              src="/Icon/minusCircle.svg"
+              alt="delete option"
+              width="30px"
+              height="30px"
+              onClick={handleRemoveInput}
+            />
+          </label>
+          <input
+            type="text"
+            {...register("OptionInputs")}
+            placeholder="write option here"
+          />
+          <input type="submit" />
         </StyledForm>
       </StyledList>
     </>
