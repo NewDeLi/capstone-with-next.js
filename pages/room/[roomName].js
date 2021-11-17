91882ea805a2595acd7b420c3d0240fe7cac930d
Denis Lindner <lindnerdenis@web.de>
2021-11-17 15:18:34 +0100

    delete this

---
+++ pages/room/[roomName].js
@@ -0,0 +1,43 @@
+import { OptionFormList } from "../components/OptionFormList";
+import VoteCard from "../components/VoteCard";
+import Result from "../components/Result";
+import Link from "next/link";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+const AddCard = ({}) => {
+  const router = useRouter();
+  console.log(router.query);
+  return (
+    <>
+      <OptionFormList />
+
+      <VoteCard />
+
+      <Result />
+
+      <StyledNav>
+        <Link href="/home">
+          <a>
+            <img src="/Icon/Home.svg" alt="home" width="50px" height="50px" />
+          </a>
+        </Link>
+      </StyledNav>
+    </>
+  );
+};
+export default AddCard;
+
+const StyledNav = styled.nav`
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  margin: 0 auto;
+  img {
+    border: 1px solid black;
+    background-color: orange;
+    border-radius: 10px;
+    padding: 0.5rem;
+    margin: 3rem 3.1rem;
+  }
+`;
