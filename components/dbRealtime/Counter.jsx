9c1556b2e9a1bc995320795d18a45ab6f048be77
NewDeLi <lindnerdenis@web.de>
2021-12-23 16:20:15 +0100

    Realtime Counter Test

--- components/dbRealtime/Counter.jsx
+++ components/dbRealtime/Counter.jsx
@@ -6,11 +6,25 @@ import Button from 'react-bootstrap/Button'
 const Counter = ({ id }) => {
     const [count, setCount] = useState('')
   
+    useEffect(() => {
+        const onCountIncrease = (count) => setCount(count.val())
+
+        const fetchData = async () => {
+            firebase.database().ref('counts').child(id).on('value', onCountIncrease)
+        }
+
+        fetchData()
+
+        return () => {
+            firebase.database().ref('counts').child(id).off('value', onCountIncrease)
+        }
+    }, [id])
 
     const increaseCount = async () => {
         const registerCount = () => fetch(`/api/incrementCount?id=${encodeURIComponent(id)}`)
         registerCount()
     }
+ 
 
     return (
         <div style={{ margin: '5px 0' }}>
