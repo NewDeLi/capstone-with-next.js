cb0df7c2842b8878c43f984f0037e19366732de4
NewDeLi <lindnerdenis@web.de>
2022-02-18 18:41:50 +0100

    change path to fetch

--- components/dbRealtime/Counter.jsx
+++ components/dbRealtime/Counter.jsx
@@ -3,6 +3,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 import Button from "react-bootstrap/Button";
 
+
 const Counter = ({ id }) => {
   const [count, setCount] = useState("");
 
@@ -30,7 +31,7 @@ const Counter = ({ id }) => {
 
   const increaseCount = async () => {
     const registerCount = () =>
-      fetch(`/api/incrementCount?id=${encodeURIComponent(id)}`);
+      fetch(`../../pages/api/incrementCount?id=${encodeURIComponent(id)}`);
     registerCount();
   };
 
